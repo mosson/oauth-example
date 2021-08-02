13a3ad71a293b6c59971c7bf1cbb855801f303e9
mosson <cucation@gmail.com>
2021-08-02 16:40:48 +0900

    fix format

--- client_server.ts
+++ client_server.ts
@@ -1,6 +1,11 @@
-import { serve, Server, ServerRequest, Response } from "https://deno.land/std@0.103.0/http/server.ts";
-import { Status } from 'https://deno.land/std@0.103.0/http/http_status.ts';
-const server: Server = serve({port: 8000});
+import {
+  Response,
+  serve,
+  Server,
+  ServerRequest,
+} from "https://deno.land/std@0.103.0/http/server.ts";
+import { Status } from "https://deno.land/std@0.103.0/http/http_status.ts";
+const server: Server = serve({ port: 8000 });
 
 console.log("Client Server running at http://localhost:8000");
 
@@ -9,7 +14,7 @@ for await (const request: ServerRequest of server) {
   const response: Response = {
     status: Status.Found,
     // Headers はトップレベルで定義されている
-    headers: new Headers([['Location', 'http://localhost:8001']])
+    headers: new Headers([["Location", "http://localhost:8001"]]),
   };
 
   request.respond(response);

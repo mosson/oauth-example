7c2e68a4e3f0669ce8314c5ea2c45ad54060504c
mosson <cucation@gmail.com>
2021-08-02 15:22:28 +0900

    initial commit

---
+++ authorization_server.ts
@@ -0,0 +1,8 @@
+import { serve, Server, ServerRequest } from "https://deno.land/std@0.103.0/http/server.ts";
+const server: Server = serve({port: 8001});
+
+console.log('Auhotirzatoin Server running at http://localhost:8001');
+
+for await (const req: ServerRequest of server) {
+  req.respond({body: "Here is Login Form"});
+}
---
+++ client_server.ts
@@ -0,0 +1,16 @@
+import { serve, Server, ServerRequest, Response } from "https://deno.land/std@0.103.0/http/server.ts";
+import { Status } from 'https://deno.land/std@0.103.0/http/http_status.ts';
+const server: Server = serve({port: 8000});
+
+console.log("Client Server running at http://localhost:8000");
+
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/for-await...of
+for await (const request: ServerRequest of server) {
+  const response: Response = {
+    status: Status.Found,
+    // Headers はトップレベルで定義されている
+    headers: new Headers([['Location', 'http://localhost:8001']])
+  };
+
+  request.respond(response);
+}

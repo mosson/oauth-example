0cb066c29fe53505a5cd6363242bc85c8ce30379
mosson <cucation@gmail.com>
2021-08-02 16:40:56 +0900

    fix format

--- authorization_server.ts
+++ authorization_server.ts
@@ -5,7 +5,7 @@ import {
 } from "https://deno.land/std@0.103.0/http/server.ts";
 const server: Server = serve({ port: 8001 });
 
-console.log("Auhotirzatoin Server running at http://localhost:8001");
+console.log("Auhotirzation Server running at http://localhost:8001");
 
 for await (const req: ServerRequest of server) {
   req.respond({ body: "Here is Login Form" });
